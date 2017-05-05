b322bad1e716387d36f496041e64d540ec3c8679
wx Zhang <wx Zhang>
2017-05-05 16:37:34 +0800

    change the count

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express'),
     fs = require('fs'),
     bodyParser = require('body-parser');
 
-var count = 1310;
+var count = 1685;
 
 var index = function(req, res) {
     count++;
